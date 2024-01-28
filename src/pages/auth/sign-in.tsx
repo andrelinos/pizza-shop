6a7e61d169eecd83c8697a46df560742c070e755
Andrelino Silva <andrelinodev@gmail.com>
2024-01-28 11:40:17 -0300

    first commit

---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
+export function SignIn() {
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="flex">
+        <h1>sign-in</h1>
+      </div>
+    </>
+  )
+}
