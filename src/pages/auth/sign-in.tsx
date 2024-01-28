c87bb003d2563455122b513d9897f19effad1f48
Andrelino Silva <andrelinodev@gmail.com>
2024-01-28 13:41:58 -0300

    Instalando e configurando Toast sonner

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,30 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInFormSchema = z.object({
+  email: z.string().email(),
+})
+
+type SignInFormProps = z.infer<typeof signInFormSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormProps>()
+
+  async function handleSignIn(data: SignInFormProps) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    toast.success('Enviamos um link de autenticação para seu e-mail.')
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +38,13 @@ export function SignIn() {
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
-          <form className="space-y-4 ">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
