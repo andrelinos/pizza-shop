4473ac5fcdf28ea89dc98bd04e1d443582a0e97e
Andrelino Silva <andrelinodev@gmail.com>
2024-04-17 07:26:50 -0300

    Loading das métricas

--- src/pages/app/dashboard/components/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/components/day-orders-amount-card.tsx
@@ -4,11 +4,14 @@ import { Utensils } from 'lucide-react'
 import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
+import { MetricCardSkeleton } from './metric-card-skeleton'
+
 export function DayOrdersAmountCard() {
-  const { data: dayOrdersAmount } = useQuery({
-    queryFn: getDayOrdersAmount,
-    queryKey: ['metrics', 'day-orders-amount'],
-  })
+  const { data: dayOrdersAmount, isFetching: isFetchingDayOrderAmount } =
+    useQuery({
+      queryFn: getDayOrdersAmount,
+      queryKey: ['metrics', 'day-orders-amount'],
+    })
 
   return (
     <Card>
@@ -17,7 +20,7 @@ export function DayOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {dayOrdersAmount && (
+        {dayOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {dayOrdersAmount.amount?.toLocaleString('pt-BR')}
@@ -40,6 +43,8 @@ export function DayOrdersAmountCard() {
               )}
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
